3a5d3e3f9feed02212519fde16485677bc2abb65
dk <65610875+dkfrede@users.noreply.github.com>
2022-09-29 18:49:16 +0200

    Setup command

--- bot/commands/setup.js
+++ bot/commands/setup.js
@@ -1,9 +1,57 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { 
+    v1: uuidv1,
+  } = require('uuid');
+  
+
 module.exports = {
-    data: {
-        name: "setup",
-        description: "Setup your log channel"
-    },
+    data: 
+        new SlashCommandBuilder()
+        .setName("setup")
+        .setDescription("Setup a part of FiveShield")
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName("key")
+                .setDescription("Setup key system"))
+        .addSubcommand(subcommand => 
+            subcommand
+                .setName("log")
+                .setDescription("Setup log system"))
+    ,
 	async execute(client, five, interaction, db) {
-        console.log(interaction)
+        if (interaction.options.getSubcommand() == 'key') {
+            if (interaction.guildId == null) {return interaction.reply("You are not in a guild.")}
+            const data = await db.prepare(
+                'SELECT * FROM `servers` WHERE `guild` = ?'
+            ).get(interaction.guildId)
+
+            if (data != null && data['key'] != "") {return interaction.reply({content: "This server already has a key - find it by using /server info", ephemeral: true})}
+
+            if (data == null) {
+                await db.prepare('INSERT INTO `servers` (`guild`, `key`, `log`) VALUES (?, ?, ?)').run(interaction.guildId, "", "")
+            }
+            
+            const key = uuidv1()
+            interaction.reply({content: "Your new key is `"+key+"` - don't share this code with anyone but your developers.", ephemeral: true})
+            
+            await db.prepare(
+                'UPDATE `servers` SET `key` = ? WHERE `guild` = ?'
+            ).run(key, interaction.guildId)
+        } else if (interaction.options.getSubcommand() == 'log') {
+            const channel = interaction.channelId
+            if (interaction.guildId == null) {return interaction.reply("You are not in a guild.")}
+            const data = await db.prepare(
+                'SELECT * FROM `servers` WHERE `guild` = ?'
+            ).get(interaction.guildId)
+
+            if (data == null) {
+                return interaction.reply({content: "Please setup your key first.", ephemeral: true})
+            }
+            db.prepare(
+                'UPDATE `servers` SET `log` = ? WHERE `guild` = ?'
+            ).run(channel, interaction.guildId)
+
+            return interaction.reply({content: "Log channel is now set to <#"+channel+"> for guild `"+interaction.guildId+"`", ephemeral: true})
+        }
 	},
 };
--- five.js
+++ five.js
@@ -11,6 +11,9 @@ class FiveShield {
     constructor() {
         this.db = new sqlite('fiveshield.sqlite')
         this.client = new discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION'],intents: 32767,disableMentions: 'everyone',})
+        const qrs=[
+            'CREATE TABLE IF NOT EXISTS `servers` (`guild` varchar(255) NOT NULL, `key` varchar(255) NOT NULL, `log` varchar(255) NOT NULL)'
+        ].forEach(e => this.db.exec(e))
     }
 
     run() {
